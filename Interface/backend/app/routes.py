f17303475d45be7c06a306efb02c6118fef19453
helderbetiol <helderbetiol@gmail.com>
2021-03-09 10:27:50 +0100

    Backend update

--- Interface/backend/app/routes.py
+++ Interface/backend/app/routes.py
@@ -1,6 +1,45 @@
 from app import app
+from influxdb import InfluxDBClient
+import json
+from flask import request
+
+HOST = 'localhost'
+PORT = 8086
+USER = 'root'
+PASSWORD = 'root'
+DBNAME = 'rabbit'
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
+
+@app.route('/lapin/<measurement>/<group_id>')
+def get_lapin(measurement, group_id):
+    print("GET_LAPIN")
+    # Database name as query param or default
+    dbname = request.args.get('dbname')
+    if not dbname:
+        dbname = DBNAME
+
+    # Connect to Database
+    client = InfluxDBClient(HOST, PORT, USER, PASSWORD, DBNAME)
+
+    # Prepare query
+    field = request.args.get('field')
+    if not field:
+        field = "*" # get all
+    query = 'select {0} from {1} where \"group\"=\'{2}\''.format(field, measurement, group_id)
+    limit = request.args.get('limit')
+    if limit:
+        query += 'limit {0};'.format(limit)
+    else:
+        query += ';'
+    print("Querying data: " + query)
+
+    # Send query and respond
+    result = client.query(query)
+    response = list(result.get_points())
+    print("Result: {0}".format(response))
+    return json.dumps(response)
+
